a5cd986aace28e54a359a3be4d6a723774a72b5d
KlasL4243 <lukasklassen4243@gmail.com>
2023-11-18 00:32:26 +0100

    restarted

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from tkinter import Tk, Label
 
-from game import Game, Fr
+from game import Game
+from homeFrame import HomeFrame
 
 class App:
     def __init__(self):
         self.tk = Tk()
         self.game = Game()
 
-        self.tk.grid_rowconfigure(0, weight=1)
-        self.tk.grid_columnconfigure(0, weight=1)
+        self.tk.grid_rowconfigure(index=0, weight=1)
+        self.tk.grid_columnconfigure(index=0, weight=1)
 
         self.width = 1080
         self.height = 2076
@@ -16,25 +17,19 @@ class App:
         self.screen_width = self.tk.winfo_screenwidth()
         self.screen_height = self.tk.winfo_screenheight()
 
-        if self.height != self.screen_height:  # not mobile
+        if self.height != self.screen_height:  # halfing size for desktop
             self.width //= 2
             self.height //= 2
 
         self.tk.geometry(f'{self.width}x{self.height}+0+0')
-
-        self.label = Label(master=self.tk)
-        #self.label.grid(row=0, column=0, sticky="nswe")
-
-        self.fr = Fr(self.tk)
-        self.fr.grid(row=0, column=0, sticky="nsew")
+        self.home_frame = HomeFrame(master=self.tk)
+        self.home_frame.grid(row=0, column=0)
 
 
 if __name__ == "__main__":
     app = App()
 
     player_list = ["Lukas", "Mirjam", "Maik", "Selina"]
-
     app.game.addPlayers(player_list)
-    app.label["text"] = ", ".join(app.game.players.keys())
 
     app.tk.mainloop()
