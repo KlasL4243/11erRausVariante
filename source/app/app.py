849ee65160f96bf7541efabd75065e0ddba3744e
KlasL4243 <lukasklassen4243@gmail.com>
2023-11-17 23:19:10 +0100

    removed ctk

--- source/app/app.py
+++ source/app/app.py
@@ -1,23 +1,22 @@
-import tkinter as tk
-import customtkinter as ctk
+from tkinter import Tk
 from source.game import Game
 from source.app.frames.homeFrame import HomeFrame
 
-class App(ctk.CTk):
+class App(Tk):
     def __init__(self):
         super().__init__()
 
-        window_width = 800
-        window_height = 500
+        # window_width = 800
+        # window_height = 500
 
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         print(screen_height, screen_width)
 
-        position_top = int(screen_height / 2 - window_height / 2)
-        position_right = int(screen_width / 2 - window_width / 2)
+        # position_top = int(screen_height / 2 - window_height / 2)
+        # position_right = int(screen_width / 2 - window_width / 2)
 
-        self.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
+        # self.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
 
         self.grid_columnconfigure(index=0, weight=1)
         self.grid_rowconfigure(index=0, weight=1)
--- source/app/frames/homeFrame.py
+++ source/app/frames/homeFrame.py
@@ -1,6 +1,7 @@
-import customtkinter as ctk
+from tkinter import Frame, Button
+from tkinter.font import Font
 
-class HomeFrame(ctk.CTkScrollableFrame):
+class HomeFrame(Frame):
     def __init__(self, master):
         super().__init__(master=master)
 
@@ -8,10 +9,10 @@ class HomeFrame(ctk.CTkScrollableFrame):
         self.grid_rowconfigure(index=0, weight=1)
         self.grid_rowconfigure(index=1, weight=1)
 
-        self.font24 = ctk.CTkFont(family="Segoe UI", size=24, weight="bold")
+        self.font24 = Font(family="Segoe UI", size=24, weight="bold")
 
-        self.new_game_button = ctk.CTkButton(master=self, font=self.font24, width=20, text="Neues Spiel")
-        self.resume_button = ctk.CTkButton(master=self, font=self.font24, width=20, text="Fortsetzen")
+        self.new_game_button = Button(master=self, font=self.font24, width=20, text="Neues Spiel")
+        self.resume_button = Button(master=self, font=self.font24, width=20, text="Fortsetzen")
 
         self.new_game_button.grid(row=0, column=0, sticky="nswe", padx=5, pady=5)
         self.resume_button.grid(row=1, column=0, sticky="nswe", padx=5, pady=5)
