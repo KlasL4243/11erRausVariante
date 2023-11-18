32e2082ebdf090481f90a574bcc10247cbc289bb
KlasL4243 <lukasklassen4243@gmail.com>
2023-11-18 20:04:29 +0100

    a lot

--- main.py
+++ main.py
@@ -36,13 +36,15 @@ class App(Tk):
         self.frame_kwargs = {"master": self, "main_font": self.main_font, "game": self.game}
 
         self.home_frame = HomeFrame(**self.frame_kwargs, is_mobile=self.is_mobile)
+        self.home_frame.grid(row=0, column=0)
+
         self.new_game_frame = NewGameFrame(**self.frame_kwargs)
         self.resume_game_frame = ResumeGameFrame(**self.frame_kwargs)
         self.names_frame = NamesFrame(**self.frame_kwargs)
         self.bets_frame = BetsFrame(**self.frame_kwargs)
 
 
-        self.home_frame.grid(row=0, column=0)
+
 
 
 if __name__ == "__main__":
