02b4f7f0e6baa9a1be6bc2f1a17dabfab01a8ab9
KlasL4243 <lukasklassen4243@gmail.com>
2023-11-18 13:03:31 +0100

    them changes

--- homeFrame.py
+++ homeFrame.py
@@ -8,8 +8,8 @@ class HomeFrame(Frame):
 
         # no expanding
 
-        self.font24 = Font(family="Segoe UI", size=(12 if self.is_mobile else 24), weight="bold")
-        self.button_width = 20
+        self.font24 = Font(family="Segoe UI", size=24, weight="bold")
+        self.button_width = (12 if self.is_mobile else 20)
 
         self.new_game_button = Button(master=self, font=self.font24, width=self.button_width, text="Neues Spiel", state="disabled")
         self.resume_button = Button(master=self, font=self.font24, width=self.button_width, text="Fortsetzen", state="disabled")
